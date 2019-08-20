309da2eaece9f1427b9bad8b957255263d8f6988
sycx0328 <sycx@sysh.tc.edu.tw>
2019-08-20 14:08:09 +0800

    新增專案

---
+++ class03-1/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  class03-1
+//
+//  Created by RTC34 on 2019/8/20.
+//  Copyright © 2019 gis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        var a = "Hi,Swift"
+        let b = "Hello! Word"
+        let c = 123
+        
+        a = a + " -> chang"
+        
+        print(a)
+        print(b)
+        print(c)
+        
+        var 到超市 = true
+        var 看到包子 = true
+        var 包子數量超過十個 = true
+        var 看到西瓜 = true
+        var 包子的數量 = 0
+        var 西瓜的數量 = 0
+        
+        if 到超市 {
+            if 包子數量超過十個
+            {
+                包子的數量 = 10
+            }
+            else
+            {
+                包子的數量 = 0
+            }
+            if 看到西瓜
+            {
+                包子的數量 = 1
+            }
+            else
+            {
+                包子的數量 = 0
+            }
+        }
+        
+        print(包子的數量)
+        print(西瓜的數量)
+        
+        let result = add(first: 1, second: 2)
+        print(result)
+        
+    }
+
+    func add(first : Int, second : Int) -> Int {
+        return first + second
+    }
+}
